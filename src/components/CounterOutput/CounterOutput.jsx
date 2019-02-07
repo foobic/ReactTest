547125a2f88fcc37f66024a60f129c252d3c2a38
foobic <foobic@gmail.com>
2019-02-07 15:59:57 +0200

    React-router basic setup

--- src/components/CounterOutput/CounterOutput.jsx
+++ src/components/CounterOutput/CounterOutput.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from '../Home/Home';
 
 import './CounterOutput.css';
 
@@ -7,6 +9,12 @@ const counterOutput = props => {
   const { value } = props;
   return (
     <div className="CounterOutput">
+      <Router>
+        <div>
+          <Link to="/home">Home</Link>
+          <Route exact path="/home" component={Home} />
+        </div>
+      </Router>
       <p>
         Current Counter:
         {value}
