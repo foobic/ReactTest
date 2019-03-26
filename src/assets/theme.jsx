935b52f4517d9a8029ff72b063153e46ccb9cc73
foobic <foobic@gmail.com>
2019-03-26 23:00:53 +0200

    Stylization

---
+++ src/assets/theme.jsx
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const Theme = createMuiTheme({
+  palette: {
+    // primary: { main: '#2196f3' },
+    primary: { main: '#fff' },
+    secondary: { main: '#2979ff' },
+  },
+});
+
+export default Theme;
--- src/index.jsx
+++ src/index.jsx
@@ -2,20 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import App from './App';
 import configureStore from './store';
 import './index.css';
 import { Firebase, FirebaseContext } from './firebase';
+import theme from './assets/theme';
 
 const store = configureStore();
 
 ReactDOM.render(
   <BrowserRouter>
-    <Provider store={store}>
-      <FirebaseContext.Provider value={new Firebase()}>
-        <App />
-      </FirebaseContext.Provider>
-    </Provider>
+    <MuiThemeProvider theme={theme}>
+      <Provider store={store}>
+        <FirebaseContext.Provider value={new Firebase()}>
+          <App />
+        </FirebaseContext.Provider>
+      </Provider>
+    </MuiThemeProvider>
   </BrowserRouter>,
 
   document.getElementById('root'),
