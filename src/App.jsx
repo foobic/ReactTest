5f36615553c51c2c34dfca9c2a14e590738d00ba
foobic <foobic@gmail.com>
2019-04-10 15:31:18 +0300

    Initialize data only once at App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Switch, Route, withRouter } from 'react-router';
-import React from 'react';
+import React, { Component } from 'react';
 import * as ROUTES from './routes';
 import { PrivateRoute } from './helpers';
 import {
@@ -10,42 +10,52 @@ import {
 } from './containers';
 import { PageNotFound } from './components';
 import { FirebaseContext } from './firebase';
+import actionCreators from './store/actionCreators';
 
-const App = props => {
-  return (
-    <FirebaseContext.Consumer>
-      {firebase => {
-        const { auth } = props.store.getState();
-        return (
-          <Switch>
-            <PrivateRoute
-              exact
-              path={ROUTES.HOME}
-              authed={!!auth.user}
-              component={() => <HomeContainer firebase={firebase} />}
-            />
-            <PrivateRoute
-              exact
-              path={ROUTES.UPLOAD}
-              authed={!!auth.user}
-              component={() => <UploadContainer firebase={firebase} />}
-            />
-            <Route
-              exact
-              path={ROUTES.ACCOUNT}
-              component={() => <AccountContainer firebase={firebase} />}
-            />
-            <Route
-              exact
-              path={ROUTES.SIGN_UP}
-              component={() => <SignupContainer firebase={firebase} />}
-            />
-            <Route component={() => <PageNotFound />} />
-          </Switch>
-        );
-      }}
-    </FirebaseContext.Consumer>
-  );
-};
+class App extends Component {
+  componentWillMount() {
+    const { store } = this.props;
+    const { auth } = store.getState();
+    if (!auth.user) store.dispatch(actionCreators.auth.loadFromLS());
+  }
+
+  render() {
+    return (
+      <FirebaseContext.Consumer>
+        {firebase => {
+          const { store } = this.props;
+          const { auth } = store.getState();
+          return (
+            <Switch>
+              <PrivateRoute
+                exact
+                path={ROUTES.HOME}
+                authed={!!auth.user}
+                component={() => <HomeContainer firebase={firebase} />}
+              />
+              <PrivateRoute
+                exact
+                path={ROUTES.UPLOAD}
+                authed={!!auth.user}
+                component={() => <UploadContainer firebase={firebase} />}
+              />
+              <Route
+                exact
+                path={ROUTES.ACCOUNT}
+                component={() => <AccountContainer firebase={firebase} />}
+              />
+              <Route
+                exact
+                path={ROUTES.SIGN_UP}
+                component={() => <SignupContainer firebase={firebase} />}
+              />
+              <Route component={() => <PageNotFound />} />
+            </Switch>
+          );
+        }}
+      </FirebaseContext.Consumer>
+    );
+  }
+}
 
 export default withRouter(App);
